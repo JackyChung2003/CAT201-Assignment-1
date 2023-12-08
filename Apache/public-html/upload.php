6b30a34df91094438b0d17fe9520aaa9bc77ffc3
Jacky Chung <clashofjacky@gmail.com>
2023-12-09 01:19:02 +0800

    added upload.php

--- Apache/public-html/upload.php
+++ Apache/public-html/upload.php
@@ -1,62 +1,28 @@
-<!-- // Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Handle file upload
-    $uploadDir = "uploads/";
-    $uploadedFile = $uploadDir . basename($_FILES["pdfFile"]["name"]);
-    $uploadOk = 1;
-    $fileType = strtolower(pathinfo($uploadedFile, PATHINFO_EXTENSION));
-    // Check if the file is a PDF
-    if ($fileType != "pdf") {
-        echo "<p style='color: red;'>Only PDF files are allowed.</p>";
-        $uploadOk = 0;
-    }
-    // Check if file already exists
-    if (file_exists($uploadedFile)) {
-        echo "<p style='color: red;'>File already exists.</p>";
-        $uploadOk = 0;
-    }
-    // Upload the file
-    if ($uploadOk) {
-        if (move_uploaded_file($_FILES["pdfFile"]["tmp_name"], $uploadedFile)) {
-            echo "<p style='color: green;'>File uploaded successfully.</p>";
-         
-            // TODO: Call Java application to convert PDF to text
-            // Use exec() function to execute the Java application
-        } else {
-            echo "<p style='color: red;'>Error uploading file.</p>";
-        }
-    }
-} -->
-
 <?php
-// Example: Log errors to a file
-// error_log('An error occurred: ' . $error_message, 3, 'error_log.txt');
+// Specify the directory where uploaded files will be stored
+$uploadDirectory = 'Upload_Files/';
+// Specify the directory where the converted file will be stored
+$outputDirectory = 'Output_Files/';
 
 // Check if the form is submitted using POST method
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Specify the directory where uploaded files will be stored
-    $uploadDirectory = 'Upload_Files/';
-
-    // Specify the directory where the converted file will be stored
-    $outputDirectory = 'Output_Files/';
-    
     // Check if files were uploaded
-    if (!empty($_FILES['pdfFiles']['name'][0])) {
+    if (!empty($_FILES['files']['name'][0])) {
+
+        // Initialize an array to store output file names + extension (ex. sample.pdf)
+        $convertedFiles = [];
+
+        $totalFiles = count($_FILES['files']['name']);
 
         // Loop through each uploaded PDF and TXT file
-        foreach ($_FILES['pdfFiles']['tmp_name'] as $key => $tmp_name) {
-            
+        foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
             // Validate file type
             // get uploaded file extension(get pdf or txt)
-            $upload_file_extension = pathinfo($_FILES['pdfFiles']['name'][$key], PATHINFO_EXTENSION);
+            $upload_file_extension = pathinfo($_FILES['files']['name'][$key], PATHINFO_EXTENSION);
             
             //Allowed file extension
-            $allowed_file_extension = ['pdf', 'csv'];
-            // if (strtolower($fileType) !== 'pdf') {
-            //     echo 'Error: Only PDF files are allowed.';
-            //     return;
-            // }
+            $allowed_file_extension = ['pdf', 'txt'];
             
             // Output if the file type is not allowed
             if (!in_array(strtolower($upload_file_extension), $allowed_file_extension)) {
@@ -65,48 +31,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
 
             // get uploaded file name with extension(.pdf or .txt)
-            $upload_file_name = $_FILES['pdfFiles']['name'][$key];
-            // $upload_file_name = $_FILES['uploadFile']['name'];
-
-
-            // if anything happen then try this
-            // $upload_file_name = $_FILES['pdfFiles']['name'][$key];
+            $upload_file_name = $_FILES['files']['name'][$key];
 
             // rename uploaded file that has spaces between the name
-            // $upload_file_name_without_space  = $uploadDirectory . str_replace(' ', '_', basename($_FILES['pdfFiles']['name'][$key]));
-            $upload_file_name_without_space = $uploadDirectory . str_replace(' ', '_', basename($upload_file_name));
+            $upload_file_name_without_space = str_replace(' ', '_', basename($upload_file_name));
 
             // check if renaming is needed
-            if ($upload_file_name_without_space != $upload_file_name) {
-                rename($upload_file_name_without_space, $upload_file_name);
+            if (trim($upload_file_name_without_space) != trim($upload_file_name)) {
+                $upload_file_name = $upload_file_name_without_space;
             }
-
+    
             // get the file name without extension(no .pdf or .txt)
             // $upload_file_name_without_extension = substr($upload_file_name_without_space, 0, -3);
             $upload_file_name_without_extension = pathinfo($upload_file_name, PATHINFO_FILENAME);
 
             // get the file temporary name
-            $upload_file_tmp_name = $_FILES['pdfFiles']['tmp_name'];
-            // $upload_file_tmp_name = $_FILES['pdfFiles']['tmp_name'][$key];
-            // echo 'Starting conversion...';
+            $upload_file_tmp_name = $_FILES['files']['tmp_name'][$key];
+
+            // set the location of the uploaded file and the converted file
+            $converted_pdf_file = $upload_file_name_without_extension . ".txt";
+            $converted_txt_file = $upload_file_name_without_extension . ".pdf";
 
             // check if the uploaded file is a PDF file or a TXT file
             if ($upload_file_extension == "pdf") {
                 // move uploaded file to upload directory
-                if (move_uploaded_file($upload_file_tmp_name, $uploadDirectory . $upload_file_name)) {
-
-                    // set the location of the uploaded file and the converted file
-                    $converted_pdf_file = $upload_file_name_without_extension . ".pdf";
+                if (move_uploaded_file($upload_file_tmp_name, $uploadDirectory. $upload_file_name)) {
                     $convert_path = $uploadDirectory . $upload_file_name;
                     $output_path = $outputDirectory . $upload_file_name_without_extension;
-                    echo 'Entering pdf loop...';
-                
-                    // convert file to CSV file using a JAR file
-                    exec("java -jar pdftotxt.jar $convert_path $output_path");
-                    echo 'Finish calling jar file...';
-                
-                    // redirect to success convert file page if conversion is successful
-                    header("Location: success_convert_file.php?converted_file={$converted_pdf_file}");
+                    $command = "java -jar /var/www/CAT_201_APACHE/pdf2txt.jar $convert_path $output_path";
+                    $output = [];
+                    $returnCode = 0;
+                    exec($command, $output, $returnCode);
+                    $convertedFiles[] = $converted_pdf_file;
+                    $output = shell_exec($command);
                 } 
                 // error if file is not moved to upload directory
                 else {
@@ -120,21 +77,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if (move_uploaded_file($upload_file_tmp_name, $uploadDirectory . $upload_file_name)) {
 
                     // set the location of the uploaded file and the converted file
-                    $converted_txt_file = $upload_file_name_without_extension . ".pdf";
+                    // $converted_txt_file = $upload_file_name_without_extension . ".pdf";
                     $convert_path = $uploadDirectory . $upload_file_name;
                     $output_path = $outputDirectory . $upload_file_name_without_extension;
                 
                     // convert file to CSV file using a JAR file
-                    exec("java -jar txttopdf.jar $convert_path $output_path");
+                    // exec("java -jar txttopdf.jar $convert_path $output_path");
+                    $command = "java -jar /var/www/CAT_201_APACHE/txt2pdf.jar $convert_path $output_path";
+                    $output = [];
+                    $returnCode = 0;
+                    exec($command, $output, $returnCode);
+
+                    $convertedFiles[] = $converted_txt_file;
+
+                    $output = shell_exec($command);
                 
                     // redirect to success convert file page if conversion is successful
-                    header("Location: success_convert_file.php?converted_file={$converted_txt_file}");
+                    // header("Location: success_convert_file.php?converted_file={$converted_txt_file}");
                 } 
                 // error if file is not moved to upload directory
                 else {
                     // if an error occurred during uploading
                     // header('Location: error.php'); // redirect to error page if an error occurs
                     echo 'Redirect to error page (error.php)';
+                    header('Location: error.php');
                 }
             }
 
@@ -144,18 +110,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo 'Invalid file type. Only PDF and TXT files are allowed.';
                 echo 'Redirect to error page (error.php)';
             }
-            
-            // Call your Java application for each file
-            // $javaCommand = "java -jar /path/to/your/java/application.jar $inputFile $outputFile";
-            // exec($javaCommand);
-            
-            // You can handle the converted text file or provide download links
-            // based on your application's logic
-        }
+         }
+
+        // Now $convertedFiles should contain all the converted file names
+        var_dump($convertedFiles);
+
+        // Serialize the array into a string
+        $serializedFiles = serialize($convertedFiles);
+
+        // Encode the serialized string to make it URL-safe
+        $encodedFiles = urlencode($serializedFiles);
         
-        echo 'Conversion completed!';
+        // pass the encoded string to the next page, then will be decoded at that page
+        header("Location: ready-to-download.php?converted_file={$encodedFiles}");
     } else {
         echo 'Please select one or more PDF files for conversion.';
     }
 }
 ?>
+
+
