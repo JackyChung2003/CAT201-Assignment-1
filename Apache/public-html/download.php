f92c443d30485f2333aba6511721351241b7d576
JACKY CHUNG SZE YUNG <116894695+JackyChung2003@users.noreply.github.com>
2023-12-09 01:42:51 +0800

    Update download.php

--- Apache/public-html/download.php
+++ Apache/public-html/download.php
@@ -20,45 +20,10 @@ if (isset($_GET['output_file'])) {
     // Set the path to the directory where the files are stored
     $outputDirectory = 'Output_Files/';
 
-    // if (class_exists('ZipArchive')) {
-    //     echo 'ZipArchive class is available.';
-    // } else {
-    //     echo 'ZipArchive class is not available.';
-    // }
     // Create a zip archive
     // $zip = new ZipArchive();
     $zip = new ZipArchive;
     $zipFilename = $outputDirectory . 'converted_files.zip';
-
-    // // // Set the full path to the file
-    // // $filepath = $outputDirectory . $filename;
-    // foreach ($convertedFiles as $filename) {
-    //     // Set the full path to the file
-    //     $filepath = $outputDirectory . $filename;
-
-    //     // echo "filepath: " . $filepath . "<br>";
-
-    //     // Check if the file exists
-    //     if (file_exists($filepath)) {
-    //         // Set the appropriate headers for file download
-
-    //         header('Content-Description: File Transfer');
-    //         header('Content-Type: application/octet-stream');
-    //         header('Content-Disposition: attachment; filename="' . basename($filepath) . '"');
-    //         header('Expires: 0');
-    //         // header('Cache-Control: must-revalidate');
-    //         header('Cache-Control: public');
-    //         header('Pragma: public');
-    //         header('Content-Length: ' . filesize($filepath));
-
-    //                     // echo "file exists<br>";
-    //         // echo "filename: " . $filename . "<br>";
-    //         readfile($filepath);
-    //     } else {
-    //         // If the file does not exist, display an error message
-    //         echo 'File not found: ' . $filename . '<br>';
-    //     }
-    // }
 
     if ($zip->open($zipFilename, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true) {
         foreach ($convertedFiles as $filename) {
@@ -96,30 +61,6 @@ if (isset($_GET['output_file'])) {
     } else {
         echo "Error creating zip archive";
     }
-
-    // Loop through each file in the array
-    // foreach ($convertedFiles as $filename) {
-    //     // Set the full path to the file
-    //     $filepath = $outputDirectory . $filename;
-
-    //     // Check if the file exists
-    //     if (file_exists($filepath)) {
-    //         // Set the appropriate headers for file download
-    //         header('Content-Description: File Transfer');
-    //         header('Content-Type: application/octet-stream');
-    //         header('Content-Disposition: attachment; filename="' . basename($filepath) . '"');
-    //         header('Expires: 0');
-    //         header('Cache-Control: must-revalidate');
-    //         header('Pragma: public');
-    //         header('Content-Length: ' . filesize($filepath));
-    //         readfile($filepath);
-    //     } else {
-    //         // If the file does not exist, display an error message
-    //         echo 'File not found: ' . $filename . '<br>';
-    //     }
-    // }
-
-    // exit;
 } else {
     // If no filename is provided, display an error message
     echo 'Filename not provided.';
